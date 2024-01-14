8f75147e9437d7dfd327d3bdfa166339375c006a
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-14 23:20:45 +0700

    Remove class on main

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import About from "./components/about"
 
 const Home = () => {
   return (
-    <main className='2xl:container mx-auto lg:w-10/12 sm:w-11/12'>
+    <main>
       <About />
     </main>
   )
