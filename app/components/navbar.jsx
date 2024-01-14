7183b9627d914e563e71dab95bc9638690f6bb91
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-14 21:42:32 +0700

    Add navbar component

---
+++ app/components/navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="2xl:container lg:w-10/12 sm:w-11/12 mx-auto flex gap-5 h-20 items-center">
+        <h1 className="mr-auto">PM.</h1>
+        <a href="">About</a>
+        <a href="">Skills</a>
+        <a href="">Projects</a>
+        <a href="">Contact</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
