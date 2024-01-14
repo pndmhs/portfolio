21d0ada3a2bab7662e921de0eb5c80053c4c496c
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-14 23:23:08 +0700

    Add background and font color

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <nav>
-      <div className="2xl:container lg:w-10/12 sm:w-11/12 mx-auto flex gap-5 h-20 items-center">
-        <h1 className="mr-auto">PM.</h1>
+    <nav className="bg-[#1a1a1a]">
+      <div className="2xl:container lg:w-10/12 sm:w-11/12 mx-auto flex gap-7 h-20 items-center text-[#F0F0F0]">
+        <h1 className="mr-auto font-medium">PM.</h1>
         <a href="">About</a>
         <a href="">Skills</a>
         <a href="">Projects</a>
