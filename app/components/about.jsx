ca0d32f41dcad2c8aea33ce6339d0d743e59e6be
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-14 21:42:23 +0700

    Add about component

---
+++ app/components/about.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <section className="flex flex-col justify-center h-[calc(100vh-5rem)]">
+      <h1>Hey, I'm <br /><div className='text-8xl my-5'>Pandu Mahesa</div></h1>
+      <p>I am a web developer with a passion for creating engaging and user-friendly websites. I have a good understanding of front end development, including HTML, CSS, JavaScript, and React. I am a quick learner and I enjoy learning new technologies and frameworks that can enhance my web development skills. I am always seeking opportunities to expand my knowledge and improve my skills, as well as to collaborate with other developers and clients on exciting projects.
+      </p>
+    </section>
+  )
+}
+
+export default About
