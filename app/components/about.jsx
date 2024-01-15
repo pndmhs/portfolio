cbf7d6cf0c6dc5c24edacd77e98b9a44ba6b8a60
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-15 08:06:22 +0700

    Remove text color class

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -6,11 +6,11 @@ const About = () => {
   return (
     <section className="bg-[#1a1a1a]">
       <div className="2xl:container mx-auto lg:w-10/12 sm:w-11/12 flex flex-col justify-center h-[calc(100vh-5rem)] ">
-        <h1 className="text-[#F0F0F0]">Hey, I'm 
+        <h2>Hey, I'm 
           <br />
           <div className={`text-8xl my-5 text-[#1A9CFF] ${dmSans.className} font-medium`}>Pandu Mahesa</div>
-        </h1>
-        <p className="text-[#F0F0F0]">I am a web developer with a passion for creating engaging and user-friendly websites. I have a good understanding of front end development, including HTML, CSS, JavaScript, and React. I am a quick learner and I enjoy learning new technologies and frameworks that can enhance my web development skills. I am always seeking opportunities to expand my knowledge and improve my skills, as well as to collaborate with other developers and clients on exciting projects.
+        </h2>
+        <p>I am a web developer with a passion for creating engaging and user-friendly websites. I have a good understanding of front end development, including HTML, CSS, JavaScript, and React. I am a quick learner and I enjoy learning new technologies and frameworks that can enhance my web development skills. I am always seeking opportunities to expand my knowledge and improve my skills, as well as to collaborate with other developers and clients on exciting projects.
         </p>
       </div>
     </section>
