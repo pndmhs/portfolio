023fa13802da632b6f242c9cbd3ce848d305af47
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-15 22:30:41 +0700

    Add max width

--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -11,7 +11,7 @@ import inkscapeIcon from "../../public/inkscape.svg"
 const Skills = () => {
   return (
     <section className="bg-black">
-      <div className="2xl:container mx-auto lg:w-10/12 sm:w-11/12 flex flex-col justify-center h-[calc(100vh-5rem)]">
+      <div className="mx-auto lg:w-10/12 sm:w-11/12 flex flex-col justify-center h-[calc(100vh-5rem)] max-w-6xl">
         <h2 className="mb-8">SKILLS</h2>
         <div className="flex gap-12 flex-wrap">
           <Image width={96} src={htmlIcon}/>
