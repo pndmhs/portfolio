959dfd9d301e0c85d3a78c1dacc04e8dcab0c960
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-15 22:28:24 +0700

    Add project component

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import About from "./components/about"
+import Projects from "./components/projects"
 import Skills from "./components/skills"
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <main>
       <About />
       <Skills />
+      <Projects />
     </main>
   )
 }
