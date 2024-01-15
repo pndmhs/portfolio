c8370299ac95e9db45fcdedc801ed96d895d1f08
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-15 08:05:49 +0700

    Change text color

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} font-light`}>
+      <body className={`${inter.className} font-light text-[#F0F0F0]`}>
         <Navbar />
         {children}
       </body>
