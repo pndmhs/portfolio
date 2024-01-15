bd2c46d9bdb7b6368e6d03b490eedc3ced7f0de3
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-15 08:06:44 +0700

    Change position to sticky

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="bg-[#1a1a1a]">
+    <nav className="bg-[#1a1a1a] sticky top-0">
       <div className="2xl:container lg:w-10/12 sm:w-11/12 mx-auto flex gap-7 h-20 items-center text-[#F0F0F0]">
         <h1 className="mr-auto font-medium">PM.</h1>
         <a href="">About</a>
