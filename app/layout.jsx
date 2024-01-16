6b3efb4e8fc6dc2bb5aa344c806f4395616fc900
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-16 10:58:05 +0700

    Add scroll-smooth class

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} font-light text-[#F0F0F0]`}>
         <Navbar />
         {children}
