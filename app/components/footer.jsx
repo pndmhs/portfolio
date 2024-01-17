57a0720fe8da4f6d063c370feb1d0a9e0467b572
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-17 15:37:08 +0700

    Create footer component

---
+++ app/components/footer.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="mx-auto w-10/12 text-center py-5">
+        <p>
+          Website design by <a
+            className="underline"
+            href="https://www.figma.com/@melocoder"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @melocoder
+          </a>
+        </p>
+        <p>&copy; 2024 Pandu Mahesa</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
